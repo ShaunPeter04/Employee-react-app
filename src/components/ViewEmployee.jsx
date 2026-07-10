abd8e6103a5b829b866e1b35c4733489596bcfba
ShaunPeter04 <shaunpeter04@gmail.com>
2026-07-10 11:36:46 +0530

    json array format added in view employee

--- src/components/ViewEmployee.jsx
+++ src/components/ViewEmployee.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
 
 const ViewEmployee = () => {
+
+    const [empdata, changeData] = useState(
+
+        [
+
+            { "empcode": "EMP-2026-001", "name": "Eleanor Vance", "phone": "+91 98765 43210", "designation": "Senior Software Engineer" },
+            { "empcode": "EMP-2026-002", "name": "Marcus Chen", "phone": "+91 91234 56789", "designation": "Product Manager" },
+            { "empcode": "EMP-2026-003", "name": "Aisha Rahman", "phone": "+91 94470 12345", "designation": "UX/UI Designer" },
+            { "empcode": "EMP-2026-004", "name": "Liam O'Connor", "phone": "+91 98950 87654", "designation": "Data Analyst" },
+            { "empcode": "EMP-2026-005", "name": "Sofia Martinez", "phone": "+91 81290 23456", "designation": "HR Specialist" },
+            { "empcode": "EMP-2026-006", "name": "David Kim", "phone": "+91 70123 45678", "designation": "Devops Engineer" },
+            { "empcode": "EMP-2026-007", "name": "Priya Patel", "phone": "+91 96330 98765", "designation": "Marketing Manager" },
+            { "empcode": "EMP-2026-008", "name": "Sofia Martinez", "phone": "+91 81290 23456", "designation": "HR Specialist" },
+            { "empcode": "EMP-2026-009", "name": "Josin", "phone": "+91 81290 23456", "designation": "HR Specialist" },
+            { "empcode": "EMP-2026-010", "name": "Vikas", "phone": "+91 81290 23456", "designation": "HR Specialist" },
+
+        ]
+    )
+
     return (
         <div>
             <NavigationBar />
@@ -15,73 +34,21 @@ const ViewEmployee = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">EMP-2026-001</th>
-                        <td>Eleanor Vance</td>
-                        <td>+91 98765 43210</td>
-                        <td>Senior Software Engineer</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">EMP-2026-002</th>
-                        <td>Marcus Chen</td>
-                        <td>+91 91234 56789</td>
-                        <td>Product Manager</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">EMP-2026-003</th>
-                        <td>Aisha Rahman</td>
-                        <td>+91 94470 12345</td>
-                        <td>UX/UI Designer</td>
-                    </tr>
 
-                    <tr>
-                        <th scope="row">EMP-2026-004</th>
-                        <td>Liam O'Connor</td>
-                        <td>+91 98950 87654</td>
-                        <td>Data Analyst</td>
-                    </tr>
+                    {empdata.map(
 
-                    <tr>
-                        <th scope="row">EMP-2026-005</th>
-                        <td>Sofia Martinez</td>
-                        <td>+91 81290 23456</td>
-                        <td>HR Specialist</td>
-                    </tr>
+                        (value, index) => (
 
-                    <tr>
-                        <th scope="row">EMP-2026-006</th>
-                        <td>David Kim</td>
-                        <td>+91 70123 45678</td>
-                        <td>DevOps Engineer</td>
-                    </tr>
+                            <tr key={value.empcode}>
+                                <td>{value.empcode}</td>
+                                <td>{value.name}</td>
+                                <td>{value.phone}</td>
+                                <td>{value.designation}</td>
+                            </tr>
+                        )
 
-                    <tr>
-                        <th scope="row">EMP-2026-007</th>
-                        <td>Priya Patel</td>
-                        <td>+91 96330 98765</td>
-                        <td>Marketing Manager</td>
-                    </tr>
 
-                    <tr>
-                        <th scope="row">EMP-2026-008</th>
-                        <td>James Wilson</td>
-                        <td>+91 85470 11223</td>
-                        <td>QA Automation Engineer</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row">EMP-2026-009</th>
-                        <td>Elena Rostova</td>
-                        <td>+91 90720 44556</td>
-                        <td>Financial Controller</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row">EMP-2026-010</th>
-                        <td>Tariq Al-Mansoor</td>
-                        <td>+91 95620 77889</td>
-                        <td>IT Support Specialist</td>
-                    </tr>
+                    )}
                 </tbody>
             </table>
 
