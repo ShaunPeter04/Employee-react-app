c6830d2e231aa6dcb6c9704563c8c465cc386f0c
ShaunPeter04 <shaunpeter04@gmail.com>
2026-07-10 11:48:13 +0530

    anchor tag changed to link tag in navigation bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -7,8 +7,8 @@ const NavigationBar = () => {
 
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
                 <div class="container-fluid" color="red">
-                    <a class="navbar-brand" href="# ">
-                        <img src="https://media.licdn.com/dms/image/v2/C560BAQES_mcXo9Tw2w/company-logo_200_200/company-logo_200_200/0/1668538388743/angellist_talent_logo?e=2147483647&v=beta&t=zXGhUXozieXU6AKOsposhgOH_eX2vwACefg2NvzwslM" width="120px" height="120px"></img></a>
+                    <Link class="navbar-brand" to="/ ">
+                        <img src="https://media.licdn.com/dms/image/v2/C560BAQES_mcXo9Tw2w/company-logo_200_200/company-logo_200_200/0/1668538388743/angellist_talent_logo?e=2147483647&v=beta&t=zXGhUXozieXU6AKOsposhgOH_eX2vwACefg2NvzwslM" width="120px" height="120px"></img></Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
